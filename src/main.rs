655ef92a5732937717663d05938df0cd42c2a390
mc_fdc <Masato.04.11.2007@gmail.com>
2022-06-28 04:36:01 +0000

    add: access log

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::middleware::Logger;
 use serde::{Deserialize, Serialize};
+use env_logger::Env;
 
 pub mod api;
 use crate::api::{main, message};
@@ -19,12 +21,15 @@ async fn index() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
     HttpServer::new(|| {
         App::new()
             .service(index)
             .service(main::index)
             .service(message::post_message)
             .service(message::get_messages)
+            .wrap(Logger::default())
+            .wrap(Logger::new("%a %{User-Agent}i"))
     })
     .bind("localhost:8080")?
     .run()
